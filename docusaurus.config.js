d63a04cecd4d2c7392ffe877e664f56d4aa1d553
PhWoich <pedrohenriquewoich@gmail.com>
2022-12-10 21:52:14 -0300

    Alterações da URL e do nome do projeto para tentar lançar página no github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Codex Arcadia',
   tagline: 'TTRPG de Fantasia Espacial',
-  url: 'https://github.com',
+  url: 'https://PhWoich.github.io',
   baseUrl: '/Codex_Arcadia/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PhWoich', // Usually your GitHub org/user name.
-  projectName: 'Codex Arcadia', // Usually your repo name.
+  projectName: 'Codex_Arcadia', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
