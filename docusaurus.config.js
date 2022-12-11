412e70beda650cf35c78a183450e855991c097ad
PhWoich <pedrohenriquewoich@gmail.com>
2022-12-10 21:24:06 -0300

    Alteração da URL do projeto

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Codex Arcadia',
   tagline: 'TTRPG de Fantasia Espacial',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://github.com',
+  baseUrl: '/PhWoich/Codex_Arcadia',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
